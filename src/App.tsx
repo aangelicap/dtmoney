de35d539eb6f9c02a73481b702743c37ee288fd8
Angelica <angelicapelizza@hotmail.com>
2021-06-04 18:58:07 -0300

    added transaction modal layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransacrionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-     <Header />
-     <Dashboard />
-     <GlobalStyle />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransacrionModal 
+        isOpen = {isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
     </>
   );
 }
